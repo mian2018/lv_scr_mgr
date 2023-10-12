4310f6d9ff7015a312ec2dd96c9b7da819848987
张 <39999734+mian2018@users.noreply.github.com>
2023-10-12 17:21:13 +0800

    Create lv_scr_mgr_port.h

---
+++ lv_scr_mgr_port.h
@@ -0,0 +1,37 @@
+/**
+  *******************************CopyRight  ************************************
+  * @file    lv_scr_mgr_port.h
+  * @author  zyf
+  * @date    2023-10-11 13:51:23
+  * @brief   &#&
+  *          
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _LV_SCR_MGR_PORT_H_
+#define _LV_SCR_MGR_PORT_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "lv_scr_mgr.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum
+{
+    eScreenUITest1 = 0,
+    eScreenUITest2,
+    eScreenUITest3
+}lv_scr_mgr_id_e;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LV_SCR_MGR_PORT_H_ */
+
+/************************ (C) COPYRIGHT ***********END OF FILE*****************/
+
+
