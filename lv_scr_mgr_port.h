8f9c209bd2111d1211f1ed42eda4588585ed06d2
张 <39999734+mian2018@users.noreply.github.com>
2023-10-18 13:47:36 +0800

    Update lv_scr_mgr_port.h

--- lv_scr_mgr_port.h
+++ lv_scr_mgr_port.h
@@ -3,7 +3,7 @@
   * @file    lv_scr_mgr_port.h
   * @author  zyf
   * @date    2023-10-11 13:51:23
-  * @brief   &#&
+  * @brief   lvgl 页面管理器 接口
   *          
   ******************************************************************************
   */
@@ -13,7 +13,7 @@
 #define _LV_SCR_MGR_PORT_H_
 
 /* Includes ------------------------------------------------------------------*/
-#include "lv_scr_mgr.h"
+#include "lvgl.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,6 +26,10 @@ typedef enum
     eScreenUITest3
 }lv_scr_mgr_id_e;
 
+#if !LV_SCR_MGR_REG_ENABLE
+uint32_t lv_scr_mgr_get_cnt(void);
+const lv_scr_mgr_handle_t** lv_scr_mgr_get_handles(void);
+#endif
 #ifdef __cplusplus
 }
 #endif
